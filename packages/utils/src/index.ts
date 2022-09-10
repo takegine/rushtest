f4a64c63d4a4987beb783331c2e7b6e4ee79303c
西索酱 <takegine@vip.qq.com>
2022-09-10 19:49:47 +0800

    更新utils

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -4,5 +4,5 @@ log.level = process.env.LOG_LEVEL ? process.env.LOG_LEVEL : 'info'
 
 log.heading = 'js-cli'
 log.addLevel('success', 2000, { fg: 'green', bold: true })
-
+console.log('@zold/utils 载入成功！')
 export { log }
